151f6eb11abf036d9d9fcffec27481124ab3cecd
PanDirolli <zaippo@msn.com>
2025-03-26 11:14:09 +0200

    Koodia jatkettu, lisätty urheilijat sekä perintä henkilö luokasta

--- Kolmastehtava/Urheilu.js
+++ Kolmastehtava/Urheilu.js
@@ -14,6 +14,37 @@ class Henkilo {
   }
 }
 
-const henkilo1 = new Henkilo("Pertti", "Eräreikä", "Pertsa", 1850);
-console.log("Kokonimi: " + henkilo1.getKokoNimi());
-console.log(henkilo1.getIka() + " Vuotias");
+class Urheilija extends Henkilo {
+  constructor(enimet, snimi, kutsumanimi, omapaino, syntymavuosi, laji, saavutukset) {
+    super(enimet, snimi, kutsumanimi, syntymavuosi);
+    this.omapaino = omapaino;
+    this.laji = laji;
+    this.saavutukset = saavutukset;
+  }
+  show() {
+    return (
+      "Nimi: " +
+      this.getKokoNimi() +
+      "\nSyntymävuosi: " +
+      this.syntymavuosi +
+      "\nIkä: " +
+      this.getIka() +
+      "\nOmapaino: " +
+      this.omapaino +
+      "\nLaji: " +
+      this.laji +
+      "\nSaavutukset: " +
+      this.saavutukset
+    );
+  }
+}
+
+let Urheilija1 = new Urheilija("Pertti", "Eräreikä", "Pertsa", 85, 1980, "Jalkapallo", "MM-kulta");
+
+let Urheilija2 = new Urheilija("Matti", "Meikäläinen", "Masa", 75, 1985, "Jääkiekko", "SM-kulta");
+
+console.log(Urheilija1.show());
+console.log(Urheilija2.show());
+// const henkilo1 = new Henkilo("Pertti", "Eräreikä", "Pertsa", 1850);
+// console.log("Kokonimi: " + henkilo1.getKokoNimi());
+// console.log(henkilo1.getIka() + " Vuotias");
